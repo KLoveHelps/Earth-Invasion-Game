406fc722afdda4e191698fccc2596d5658790f60
Kristal Hampton <130312928+KLoveHelps@users.noreply.github.com>
2023-09-05 08:44:15 -0500

    reverted some code that did not work
    Reverted code

--- Earth Invasion/Assets/Scripts/CollisionHandeler.cs
+++ Earth Invasion/Assets/Scripts/CollisionHandeler.cs
@@ -10,66 +10,40 @@ using UnityEngine.UI;
 public class CollisionHandeler : MonoBehaviour
 {
     AudioSource audioSource;
-    
+
     [SerializeField] float CodeDelay = 2f;
-    [SerializeField] GameObject deathVFX;
-    [SerializeField] GameObject hitVFX;
 
     [SerializeField] AudioClip win;
     [SerializeField] ParticleSystem winParticles;
-    [SerializeField] int PlayerhitPoints = 25;
+
     [SerializeField] AudioClip death;
     [SerializeField] ParticleSystem looseParticles;
 
-    
+
     bool isTransitioning = false;
     bool CollisionDisabled = false;
     public GameObject GameOverUI;
     public GameObject WinUI;
     public GameObject ScoreUI;
-   
-    ScoreBoard scoreBoard;
-    GameObject parentGameObject;
+
     ScoreBoard scoreBoardScript;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         scoreBoardScript = GameObject.FindGameObjectWithTag("ScoreVal").GetComponent<ScoreBoard>();
-        
+
     }
 
     void OnCollisionEnter(Collision other)
     {
-        ProcessHitPlayer();
-        if (PlayerhitPoints < 1)
-        {
-            killPlayer();
-        }
-        else
-        {
-            Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
-        }
-
+        Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
     }
-    void ProcessHitPlayer()
-    {
-        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parentGameObject.transform;
-        PlayerhitPoints--;
-    }
-    void killPlayer()
-    {
-        audioSource.PlayOneShot(death);
-        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parentGameObject.transform;
-        Destroy(gameObject);
-    }
-        private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (isTransitioning || CollisionDisabled)
-        { 
-            return; 
+        {
+            return;
         }
         switch (other.gameObject.tag)
         {
@@ -78,9 +52,8 @@ public class CollisionHandeler : MonoBehaviour
                 break;
             case "Win":
                 CheckScore();
-                break;
-            
 
+                break;
             default:
                 StartGameOverSequence();
                 break;
@@ -98,7 +71,7 @@ public class CollisionHandeler : MonoBehaviour
             StartWinSequence();
         }
 
-    }    
+    }
     void StartWinSequence()
     {
         isTransitioning = true;
@@ -106,24 +79,24 @@ public class CollisionHandeler : MonoBehaviour
         audioSource.PlayOneShot(win);
         WinUI.SetActive(true);
         GetComponent<PlayerControler>().enabled = false;
-        
+
     }
     void StartGameOverSequence()
     {
         isTransitioning = true;
         GetComponent<MeshRenderer>().enabled = false;
         GetComponent<MeshCollider>().enabled = false;
-        
+
         looseParticles.Play();
         audioSource.PlayOneShot(death);
         GameOverUI.SetActive(true);
         Invoke("turnoffplayerinput", CodeDelay);
-        
 
 
-        
+
+
     }
- 
+
 
     void turnoffplayerinput()
     {
